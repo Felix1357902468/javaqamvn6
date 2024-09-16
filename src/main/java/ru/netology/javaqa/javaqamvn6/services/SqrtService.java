ae7dc3c2c572270c0308bc1d945dd165a1903c02
Felix1357902468 <nik2804angel@gmail.com>
2024-09-16 14:20:40 +0700

    initial commit

--- src/main/java/ru/netology/javaqa/javaqamvn6/services/SqrtService.java
+++ src/main/java/ru/netology/javaqa/javaqamvn6/services/SqrtService.java
@@ -1,23 +1,22 @@
 package ru.netology.javaqa.javaqamvn6.services;
 
 
-    public class SqrtService {
+public class SqrtService {
 
-        public int calculate(int income, int expenses, int threshold) {
-            int count = 0; // счётчик месяцев отдыха
-            int money = 0; // количество денег на счету
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
 
-            for (int month = 0; month < 12; month++) {
-                if (money >= threshold) { // можем ли отдыхать?
-                    count++; // увеличиваем счётчик месяцев отдыха
-                    money = money - expenses;
-                    System.out.println("Месяц " +month + "." + "Денег " +money + "." + "Буду отдыхать." + "Заработал " + "+" + income + "," + "Потратил " + "-" + expenses);
-                    break; // exit the loop if the threshold is reached
-                } else {
-                    money = money + income;
-                    System.out.println("Месяц " +month + "." + "Денег " +money + "." + "Придется работать." + "Заработал " + "+" + income + "," + "Потратил " + "-" + expenses);
-                }
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses - (money - expenses) / 3; // уменьшаем деньги на траты на отдых
+                System.out.println("Месяц " + month + "." + "Денег " + money + "." + "Буду отдыхать." + "Потратил " + "-" + expenses + ", траты на отдых " + "-" + (money - expenses) / 3);
+            } else {
+                money = money + income - expenses; // увеличиваем деньги на доход от работы и уменьшаем на обязательные траты
+                System.out.println("Месяц " + month + "." + "Денег " + money + "." + "Придется работать." + "Заработал " + "+" + income + "," + "Потратил " + "-" + expenses);
             }
-            return count;
         }
+        return count;
     }
+}
